73c485508c5e8207ff045fa2b3d1199ca51d51b0
Erin Spiceland <espiceland@digium.com>
2015-06-07 18:01:05 -0500

    Got it parsing the description.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var cheerio = require('cheerio');
 
 var lessonUrl = "http://www.choctawschool.com/lesson-of-the-day.aspx";
 
@@ -12,5 +13,22 @@ request(lessonUrl, function (error, response, body) {
 });
 
 function processLesson(body) {
-    console.log(body);
+    var $ = cheerio.load(body);
+    var children = $("#aspnetform")[0].children;
+    var tag;
+    var text = "";
+
+    function processChild(each) {
+        if (each.type === "text" && each.data.match(/\S/)) {
+            each.data.replace(/[\n\r]/i, "");
+            text += each.data + "\n";
+        }
+
+        if (each.children) {
+            each.children.forEach(processChild);
+        }
+    }
+
+    children.forEach(processChild);
+    console.log(text);
 }
