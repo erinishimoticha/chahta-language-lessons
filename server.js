296136afc979a5840c25f164a92cac14c2618b02
Erin Spiceland <espiceland@digium.com>
2015-06-18 19:41:54 -0500

    Add a route for listing lessons.

--- server.js
+++ server.js
@@ -9,7 +9,23 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
+/**
+ * Return the lessons we saved from choctawschools.com.
+ */
+app.get("/lessons", function (req, res) {
+    var lessons;
+    try {
+        lessons = require("./pageinfo.json");
+        res.json(Object.keys(lessons).map(function values(key) {
+            return lessons[key];
+        }));
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+});
+
 app.use(express.static(__dirname + '/static'));
+
 app.listen(port, function () {
     console.log("Listening on port " + port);
 });
