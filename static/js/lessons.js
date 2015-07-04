dae812e90c856721437573ebf7d2fda8a2626f90
Erin Spiceland <espiceland@digium.com>
2015-07-04 15:10:20 -0500

    Beginnings using Angular to display list of lessons.

---
+++ static/js/lessons.js
@@ -0,0 +1,12 @@
+var app = angular.module('ChoctawLessons', ['ngResource']);
+
+app.factory("Lessons", function ($resource) {
+    return $resource("/lessons");
+});
+
+app.controller('LessonsCtrl', function ($scope, Lessons) {
+    Lessons.query(function (lessons) {
+        console.log(lessons);
+        $scope.lessons = lessons;
+    });
+});
